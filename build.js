c706acd1fa1679e95edd3b7e3990037be07cddd4
Julien Bisconti <julien.bisconti@gmail.com>
2018-03-18 15:01:47 +0100

    refactor build step

--- build.js
+++ build.js
@@ -6,7 +6,7 @@ const critical = require('critical');
 
 process.env.NODE_ENV = 'production';
 
-const main = () => {
+const includeReadme = () => {
   const converter = new showdown.Converter({
     omitExtraWLInCodeBlocks: true,
     simplifiedAutoLink: true,
@@ -38,39 +38,49 @@ const main = () => {
 
   console.log('Writing index.html');
   fs.writeFileSync('website/index.merged.html', $.html(), 'utf8');
+  return { base: 'website/', src: 'index.merged.html' };
+};
 
+const css = ({ base, src }) => {
   console.log('');
   console.log('Generating critical css above the fold');
   console.log('');
+  const dimensions = [
+    {
+      height: 200,
+      width: 500
+    },
+    {
+      height: 900,
+      width: 1200
+    }
+  ];
+  const options = {
+    inline: true,
+    base,
+    src,
+    dest: 'index.html',
+    dimensions
+  };
 
-  critical
-    .generate({
-      inline: true,
-      base: 'website/',
-      src: 'index.merged.html',
-      dest: 'index.html',
-      css: 'website/style.css',
-      dimensions: [
-        {
-          height: 200,
-          width: 500
-        },
-        {
-          height: 900,
-          width: 1200
-        }
-      ]
-    })
-    .then(() => {
-      console.log('Bundling with Parcel.js');
-      console.log('');
+  return critical.generate(options);
+};
 
-      new Parcel('website/index.html', {
-        name: 'build',
-        // publicURL: '/awesome-docker'
-        publicURL: '/'
-      }).bundle();
-    });
+const bundle = () => {
+  console.log('');
+  console.log('Bundling with Parcel.js');
+  console.log('');
+
+  new Parcel('website/index.html', {
+    name: 'build',
+    publicURL: '/'
+  }).bundle();
+};
+
+const main = async () => {
+  const { base, src } = includeReadme();
+  await css({ base, src });
+  bundle();
 };
 
 main();
